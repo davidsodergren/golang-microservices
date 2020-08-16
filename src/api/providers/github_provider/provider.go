c1662567c4bf29753421197672ecbecf519f8783
david.sodergren <david.sodergren@cepheid.com>
2020-08-16 22:49:34 +0200

    more refactoring

--- src/api/providers/github_provider/provider.go
+++ src/api/providers/github_provider/provider.go
@@ -27,23 +27,20 @@ func CreateRepo(headers http.Header, request github.CreateRepoRequest) (*github.
 	if err != nil {
 		return nil, err
 	}
-	bytes, err := httpResponseToJson(response)
+
+	httpResponseInBytes, err := httpResponseToBytes(response)
 	if err != nil {
 		return nil, err
 	}
 	defer response.Body.Close()
 
-	if response.StatusCode > 299 {
-		var errResponse github.ErrorResponse
-		if err := json.Unmarshal(bytes, &errResponse); err != nil {
-			return nil, &github.ErrorResponse{StatusCode: http.StatusInternalServerError, Message: "invalid json response body"}
-		}
-		errResponse.StatusCode = response.StatusCode
-		return nil, &errResponse
+	if err := statusCodeOk(response, httpResponseInBytes); err != nil {
+		return nil, err
 	}
 
 	var result github.CreateRepoResponse
-	if err := json.Unmarshal(bytes, &result); err != nil {
+
+	if err := json.Unmarshal(httpResponseInBytes, &result); err != nil {
 		log.Println(fmt.Sprintf("error when trying to unmarshal create repo successful response: %s", err.Error()))
 		return nil, &github.ErrorResponse{StatusCode: http.StatusInternalServerError, Message: "error when trying to unmarshal github create repo response "}
 	}
@@ -57,11 +54,22 @@ func createHeader(accessToken string) http.Header {
 	return headers
 }
 
-func httpResponseToJson(response *http.Response) ([]byte, error){
+func httpResponseToBytes(response *http.Response) ([]byte, error){
 	bytes, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		log.Println(fmt.Sprintf("error when trying to unmarshal create repo successful response: %s", err.Error()))
 		return nil, &github.ErrorResponse{StatusCode: http.StatusInternalServerError, Message: "Invalid response body"}
 	}
 	return bytes, nil
+}
+
+func statusCodeOk(response *http.Response, httpResponseInBytes []byte ) error {
+	if response.StatusCode > 299 {
+		var errResponse github.ErrorResponse
+		if err := json.Unmarshal(httpResponseInBytes, &errResponse); err != nil {
+			return &github.ErrorResponse{StatusCode: http.StatusInternalServerError, Message: "invalid json response body"}
+		}
+		errResponse.StatusCode = response.StatusCode
+		return &errResponse
+	}
 }
